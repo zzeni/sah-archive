192f229296e5f2ca0f5414b37c4e68fcd1c77270
nveleva <nveskova@gmail.com>
2015-11-23 19:33:17 +0200

    add homework12 and bonus_task

---
+++ group2/nevena/L12/funtion_birthdate.js
@@ -0,0 +1,37 @@
+function calcAge(birthdate){
+
+	    var dateToday = new Date();
+        var yearNow = dateToday.getFullYear();
+        var monthNow = dateToday.getMonth() + 1;
+        var dateNow = dateToday.getDate();
+        var mdNow = monthNow + " " + dateNow;
+        
+        var bd = new Date(birthdate);
+        var yearBd = bd.getFullYear();
+        var monthBd = bd.getMonth() + 1;
+        var dateBd = bd.getDate();
+
+        var mdBd = monthBd + " " + dateBd;
+
+        var age = yearNow - yearBd;
+        console.log(age);
+
+          if(mdBd === mdNow){
+            console.log("Happy birthday!");
+          }
+          else if(monthBd < monthNow){
+            console.log("1 year more!");
+          }
+          else if(monthBd == monthNow){
+            if (dateBd < dateNow) {
+              console.log("1 year more!");
+            }
+              else{
+                console.log("You have time to your birthday!");
+              }
+          }
+          else{
+            console.log("You have time to your birthday!");
+          }
+}
+calcAge("1988-12-30");
