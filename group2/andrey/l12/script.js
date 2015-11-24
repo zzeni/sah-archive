82051ffb9886d454b35be5f2c38592d3f1c53ff8
Andrey-89 <andrey.popov1989@gmail.com>
2015-11-24 16:31:47 +0200

    Homework L12

---
+++ group2/andrey/l12/script.js
@@ -0,0 +1,19 @@
+function calcAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    if (age == 0 && today.getFullYear() == birthDate.getFullYear() && today.getMonth() == birthDate.getMonth() && today.getDate() < birthDate.getDate()) {
+        age += " Happy Birthday";
+    }
+    if (today.getFullYear() < birthDate.getFullYear() || today.getMonth() < birthDate.getMonth() || today.getDate() < birthDate.getDate()) {
+        age = "Please enter valid birth date";
+    }
+    if (birthDate.getFullYear() < 1910) {
+        age = "Please enter valid birth date";
+    }
+    return age;
+}
