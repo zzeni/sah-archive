e2bb9d912847b5dd7997cd8281bcc2f38d9d72b3
Latchezar <latchibg@gmail.com>
2015-11-24 19:09:08 +0200

    More changes(L12)

--- group1/latchezar/L12/script.js
+++ group1/latchezar/L12/script.js
@@ -4,21 +4,17 @@ function tellAge(dateofbirth) {
     var age = today.getFullYear() - birthDate.getFullYear();
     var month = today.getMonth() - birthDate.getMonth();
     var date = today.getDate() - birthDate.getDate();
-    if (age == 0) {
-        if (month == 0) {
-            if (date == 0) {
-                age="Happy birthday";
-            }
-        }
-    }
-    else if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
         age--;
   }
-    if (age < 0) {
+    else if (age < 0) {
         return "Are you from the future?";
     }
-    else if (age > 99) {
+    if (age > 99) {
         return "You are ancient!";
+    }
+    else if (month == 0 && date == 0) {
+        return "Happy Birthday!";
     }
     else {
         return age;
