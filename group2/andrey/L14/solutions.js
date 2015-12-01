dc108d0c5bd2aefba986b24ef0981e9b12b89cb1
Andrey Popov <andrey.popov1989@gmail.com>
2015-12-01 19:00:19 +0200

    some fixes

--- group2/andrey/L14/solutions.js
+++ group2/andrey/L14/solutions.js
@@ -1,46 +1,45 @@
 "use strict";
 
 function greeting(name) {
-
-    return "Hi, " + name + "!";
+    
+    return "Hello, " + name + "!";
 }
 
-function personGreeting(person, beFormal) {
+function personGreeting(Person, beFormal) {
     if (beFormal === true) {
-        return "Hello, " + person.firstName + " " + person.lastName;
+        return "Hello, " + Person.firstName + " " + Person.lastName + "!";
     } else if (beFormal === false) {
-        return "Hello, " + person.firstName;
+        return "Hello, " + Person.firstName + "!";
     }
 }
 
-class person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.isPolite = true;
-        this.greet = function (otherPerson) {
-            if (otherPerson != undefined) {
-                return "Hi, " + otherPerson.firstName + "!";
-            } else {
-                return "Hi!";
-            }
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.isPolite = true;
+    this.greet = function (otherPerson) {
+        if (otherPerson !== undefined) {
+            return "Hi, " + otherPerson.firstName + "!";
+        } else {
+            return "Hi!";
         }
+    };
 
-        this.bePolite = function (parameter) {
-            if (parameter == false) {
-                this.isPolite = false;
-            } else if (parameter == true) {
-                this.isPolite = true;
-            }
+    this.bePolite = function (parameter) {
+        if (parameter === false) {
+            this.isPolite = false;
+        } else if (parameter === true) {
+            this.isPolite = true;
         }
-        this.introduce = function (otherPerson) {
-            if (this.isPolite == true) {
-                return "Hello, my name is " + this.firstName + " " + this.lastName + "!";
-            } else if (this.isPolite == false || otherPerson != undefined) {
-                return "Hi, " + otherPerson.firstName + ", i`m " + this.firstName + "!";
-            } else if (this.isPolite == false) {
-                return "Hi, i`m " + this.firstName + "!";
-            }
+    };
+    this.introduce = function (otherPerson) {
+        if (this.isPolite === true) {
+            return "Hello, my name is " + this.firstName + " " + this.lastName + "!";
+        } else if (this.isPolite === false || otherPerson !== undefined) {
+            return "Hi, " + otherPerson.firstName + ", i`m " + this.firstName + "!";
+        } else if (this.isPolite === false) {
+            return "Hi, i`m " + this.firstName + "!";
         }
-    }
+    };
+
 }
