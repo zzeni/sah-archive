f2ffeb2509eb3410d43e239520b5dfa9c3a19877
gaFace <onchevivan@gmail.com>
2015-12-03 18:28:09 +0200

    L15 changed

--- group2/ivan/L15/validation.js
+++ group2/ivan/L15/validation.js
@@ -18,7 +18,7 @@
                 throw "You have to enter your Name!";
             } else if (email === undefined || String(email) === "") {
                 throw "You have to enter your Email!";
-            } else if ((email.indexOf("@") < 1) || (email.lastIndexOf(".") < email.indexOf("@") + 2) || (email.lastIndexOf(".")) > (email.length - 1)) {
+            } else if ((email.indexOf("@") < 1) || (email.lastIndexOf(".") < email.indexOf("@") + 2) || (email.lastIndexOf(".")) == (email.length - 1)) {
                 throw "You have to enter a valid email!";
             } else {
                 throw noteError.innerHTML;
