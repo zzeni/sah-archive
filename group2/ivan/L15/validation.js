b7dcd66a7e8de4c06ec733acc3348166f664f5ee
gaFace <onchevivan@gmail.com>
2015-12-03 11:02:42 +0200

    add L15

---
+++ group2/ivan/L15/validation.js
@@ -0,0 +1,31 @@
+(function () {
+    "use strict";
+
+    document.getElementById("submit").addEventListener('click', function (e) {
+        e.preventDefault();
+
+        var name, email, noteError;
+
+        name = document.getElementById("name").value;
+        email = document.getElementById("email").value;
+        
+        noteError = document.getElementById("placeError");
+        noteError.innerHTML = "";
+
+
+        try {
+            if (name === undefined || String(name) === "") {
+                throw "You have to enter your Name!";
+            } else if (email === undefined || String(email) === "") {
+                throw "You have to enter your Email!";
+            } else if ((email.indexOf("@") < 1) || (email.lastIndexOf(".") < email.indexOf("@") + 2) || (email.lastIndexOf(".")) > (email.length - 1)) {
+                throw "You have to enter a valid email!";
+            } else {
+                throw noteError.innerHTML;
+            }
+
+        } catch (error) {
+            noteError.innerHTML = error;
+        }
+    });
+})();
