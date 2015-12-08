8b96e5e7abedf85a3cf4f3c11cd2f44f233eccf1
Kalin <anastasovkalin@gmail.com>
2015-12-08 10:36:03 +0200

    Update homeworks
    L14

--- group2/Kalin/L12/script.js
+++ group2/Kalin/L12/script.js
@@ -7,12 +7,12 @@ function calcAge(input) {
     var month = currentDate.getMonth() - birthDate.getMonth();
     var day = currentDate.getDate() - birthDate.getDate();
 
-    if ((month == 0 && day == 0) && (yourAge >= 0)) {
+    if ((month === 0 && day === 0) && (yourAge >= 0)) {
         return "Happy Birthday!";
     } else if (yourAge > 100) {
         return "You are immortal";
     } else if (yourAge <= 0) {
-        if (yourAge == 0) {
+        if (yourAge === 0) {
             return "You are a baby!";
         }
         return "You will be on " + (yourAge * (-1));
