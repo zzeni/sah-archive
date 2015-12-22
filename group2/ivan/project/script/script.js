474926f5a55bc8eb5b00fdf670a52e3facd9ff07
gaFace <onchevivan@gmail.com>
2015-12-22 22:56:43 +0200

    shrinking navbar

--- group2/ivan/project/script/script.js
+++ group2/ivan/project/script/script.js
@@ -1,9 +1,32 @@
-//$(document).ready(function(){
-// $("body").empty();
-
-//});
 (function () {
+//===============================================================================//
+//shrinking navbar start
+	$(function () {
+		$('#header_nav').data('size', 'big');
+	});
 
+	$(window).scroll(function () {
+		if ($(document).scrollTop() > 0) {
+			if ($('#header_nav').data('size') == 'big') {
+				$('#header_nav').data('size', 'small');
+				$('#header_nav').stop().animate({
+					minHeight: '2.14em',
+					padding: '0'
+				}, 800);
+			}
+		} else {
+			if ($('#header_nav').data('size') == 'small') {
+				$('#header_nav').data('size', 'big');
+				$('#header_nav').stop().animate({
+					maxHeight: '6.14em',
+					padding: '1.5em 0'
+				}, 800);
+			}
+		}
+	});
+//shrinking navbar end
+//===============================================================================//	
+//navbar navigation start
 	$('.leftNavbar a').click(function (e) {
 		e.preventDefault();
 		$('.leftNavbar a').removeClass('active');
@@ -12,16 +35,22 @@
 		$('body > div').hide();
 		$('#' + target).show();
 	});
-
+//navbar navigation end	
+//===============================================================================//
+// accordion and filter start
 	$('#accordion1 a').click(function (e) {
 		e.preventDefault();
 		$('#accordion1 a').removeClass('current');
 		$(this).addClass('current');
 		list($(this).attr("data-filter"));
 	});
+	
 	$('#productsFilter').change(function (e) {
 		list($('#accordion1 a.current').attr("data-filter"), $(this).val());
 	});
+// accordion and filter end	
+//===============================================================================//	
+// item alone start	
 	$("#products").on("click", "img", function () {
 		var url = $(this).attr("src");
 		$("#products").empty();
@@ -31,8 +60,10 @@
 				item.toModal();
 		}
 	});
+	//item  alone end
 })();
-
+//================================================================================//
+// accordion and filter helper function start
 function list(group, sort) {
 	if (typeof sort !== 'undefined')
 		itemsRoom = sortBy(itemsRoom, sort);
@@ -43,7 +74,9 @@ function list(group, sort) {
 			item.toHtml();
 	};
 }
-
+// accordion and filter helper function end
+//=================================================================================//
+// Item start
 function Item(pic, name, price, group, kind, desc) {
 	this.picture = pic;
 	this.name = name;
@@ -58,7 +91,9 @@ function Item(pic, name, price, group, kind, desc) {
 		$("#products").append('<div class="row"><div class="col-sm-6 col-lg-6 col-md-6"><div class="panel"><div class="panel-body"><img src="' + this.picture + '" class="img-responsive center-block image-alone" alt="Image"></div></div></div><div class="col-sm-6 col-lg-6 col-md-6"><div class="panel"><div class="panel-heading panel-primary"><h3>' + this.name + '</h3></div><div class="panel-body body-alone"><p>' + this.group + ' glass /<span> type: ' + this.kind + '</span></p><p class="lead">Description:</p><p>' + this.description + '</p></div><div class="panel-footer panel-primary"><span class="price">' + this.price + '$</span></div></div></div></div>');
 	}
 }
-
+//Item end
+//=======================================================================================//
+// filter helper function start
 function sortBy(itemsList, rule) {
 	var compare;
 	switch (rule) {
@@ -88,3 +123,5 @@ function sortBy(itemsList, rule) {
 
 	return itemsList.sort(compare);
 }
+// filter helper function end
+//===================================================================================//
