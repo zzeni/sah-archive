2df18253ed99ee81798c00e913f7b062e034608f
Zhanina <zh.terziyska@abv.bg>
2016-01-07 10:52:13 +0200

    proj

--- group1/zhanina/project/portfolioproject/js/lang.js
+++ group1/zhanina/project/portfolioproject/js/lang.js
@@ -4,13 +4,24 @@ $('#langSelect').change(function () {
     $('.lang-bg, .lang-en').hide(); //$('.lang-*).hide();
     $('.lang-' + lang).show();
 
-    document.cookie = "language " + lang;
+    document.cookie = "language=" + lang;
     console.log(lang);
 });
-var allCookies = document.cookie.split(' ');
-for (var i = 0; i < allCookies.length; i++) {
-    var cookie = allCookies[i];
-    if (cookie.match(/^language/)) {
-        $('#langSelect').change();
+
+function getLangFromCookie() {
+    var result;
+    var allCookies = document.cookie.split(' ');
+    for (var i = 0; i < allCookies.length; i++) {
+        var cookie = allCookies[i];
+        if (cookie.match(/^language=/)) {
+            result = cookie.split('=')[1];
+        }
     }
+    return result;
 }
+
+var language = getLangFromCookie();
+if (language === undefined) language = "en";
+
+$('#langSelect').val(language);
+$('#langSelect').change();
