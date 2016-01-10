b57b780c5e0f23d4c44e8671c01675d5ad9944ed
Dimov89 <ilkodimov52@gmail.com>
2016-01-11 00:14:01 +0200

    second try

---
+++ group1/Ilko/lesson 18/script.js
@@ -0,0 +1,48 @@
+$(document).hover(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+$('form').find('[name=username]').blur(function (e) {
+  e.preventDefault();
+
+  var ajax = $.ajax({
+    url: "http://zenifytheweb.com/api/examples/check_username",
+    method: 'get',
+    data: { username: $(this).val() }
+  });
+
+  ajax.done(function(response) {
+    if (response === "available") {
+      console.log("OK!");
+    } else {
+      console.log("This username is taken");
+    }
+  });
+
+  ajax.fail(function(error) {
+    console.log(error);
+  });
+});
+
+$('form').find('[name=email]').blur(function (e) {
+  e.preventDefault();
+
+  var ajax = $.ajax({
+    url: "http://zenifytheweb.com/api/examples/check_username",
+    method: 'get',
+    data: { email: $(this).val() }
+  });
+
+  ajax.done(function(response) {
+    if (response === "available") {
+      console.log("OK!");
+    } else {
+      console.log("This username is taken");
+    }
+  });
+
+  ajax.fail(function(error) {
+    console.log(error);
+  });
+});
