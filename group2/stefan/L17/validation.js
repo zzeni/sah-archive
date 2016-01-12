1aabb0a405c4119d6baa3ebf710bb7bb223dde1f
StevenPopov <steven.popov@abv.bg>
2016-01-12 17:04:14 +0200

    Last Homeworks

---
+++ group2/stefan/L17/validation.js
@@ -0,0 +1,124 @@
+'use strict';
+
+$('form').submit(function (event) {
+
+
+	var form = event.target;
+
+	try {
+		fieldValidate(form, 'Name');
+	fieldValidate(form, 'Email');
+	fieldValidate(form, 'Password');
+	fieldValidate(form, 'passwordConf');
+	fieldValidate(form, 'imgURL');
+		checkName();
+		checkEmail();
+		checkPass();
+		passConfirm();
+		checkUrl();
+	} catch (error) {
+		event.preventDefault();
+		if (error.name === "FieldValidationError") {
+			$("#errorLog").html(error.message);
+			error.target.focus();
+			error.target.addClass('error');
+		} else {
+			throw error;
+		}
+
+	}
+});
+
+function formValidate(form) {
+	
+}
+var error
+var regExpCheck;
+
+function fieldValidate(form, fieldName) {
+	var fieldInput = $(form).find("input[name=" + fieldName + "]");
+	var fieldValue = fieldInput.val();
+	if (fieldValue === "") {
+		error = new Error(fieldName + " is mandatory!");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
+
+function nameValid() {
+	var fieldInput = $("#name");
+	var name = $("#name").val();
+	regExpCheck = /^[A-Za-z\s?]+$/;
+	if (!name.match(regExpCheck)) {
+		error = new Error("Please enter a valid Name");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
+
+function emailValid() {
+	var fieldInput = $("#email");
+	var email = $("#email").val();
+	regExpCheck = /^[A-Za-z][\w\.\-\_\d]+@[a-z]+[\.][a-z]+$/;
+	if (!email.match(regExpCheck)) {
+		error = new Error("Please enter a valid Email");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
+
+function passwordValid() {
+	var fieldInput = $("#password");
+	var password = $("#password").val();
+	regExpCheck = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/;
+	if (!password.match(regExpCheck)) {
+		error = new Error("Please enter a valid Password");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
+
+function passwordConfValid() {
+	var fieldInput = $("#passwordConf");
+	var password = $("#password").val();
+	if (fieldInput.val() !== password) {
+		error = new Error("Passwords must match");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
+
+function picValid() {
+	var fieldInput = $("#imgUrl");
+	var picture = $("#imgUrl").val();
+	regExpCheck = /.*/;
+	if (!picture.match(regExpCheck)) {
+		error = new Error("Please enter a valid Url");
+		error.name = "FieldValidationError";
+		error.target = fieldInput;
+		throw error;
+	} else {
+		fieldInput.removeClass('error');
+		$("#errorLog").html("");
+	}
+}
