532a9d05364c5da4fa407ec09e44d40f5bf94cb5
valyo888 <valyo888@abv.bg>
2016-05-14 23:44:47 -0700

    Update script.js

--- valentin/L12/script.js
+++ valentin/L12/script.js
@@ -1,72 +1,45 @@
 function any(list, value) {
-      if (list.indexOf(value) > -1) {
-        return true;
-      } else {
-        return false;
-      }
+	for (var i = 0; i < list.length; i++){
+		if (list[i] === value){
+			return true;
+		}
+	}
+return false;
 }
-
 
 function all(list, value) {
-    var counter = 0;
-    for (var i = 0; i < list.length; i++) {
-        if (list[i] === value) {
-            counter++;
-        }else {
-            return false;
-        }
-    } 
-    
-    if ((counter === list.length) && (list.length !== 0)){
-         return true;
-    } else {
-         return false;
-    }
-    
+	for (var i = 0; i < list.length; i++){
+		if (list[i] !== value){
+			return false;
+		}
+	}
+return true;
 }
 
 
-function callback(list, value) {
-    var counter = 0;
-    for (var i = 0; i < list.length; i++) {
-        if (list[i] === value) {
-            counter++;
-        }
-    } 
-    
-    if  ((counter === list.length) && (list.length !== 0)) {
-         return 2;
-         
-    } else if ((counter > 0) && (list.length !== 0)){
-         return 1;
-    } else {
-         return 0;
-    }
+function callback(a, b){
+		if (a === b) {
+			return true;
+		} else {
+			return false;
+		}
 }
 
-
-function hofAny(callback, array) {
-
-for (var i = 0; i < array.length; i++) {
-if ((callback(array, array[i]) === 1) || (callback(array, array[i]) === 2)) {
-    return true;
- } else if (callback(array, array[i]) === 0){
-    return false;
- } else {
-    return false;
- }
-   }
+function hofany(list, value, callback){
+	for (var i = 0; i < list.length;i++){
+		if (callback(list[i], value) === true) {
+			return true;
+		}
+	}
+	return false;
 }
 
-
-function hofAll(callback, array) {
-   for (var i = 0; i < array.length; i++) {
-    if (callback(array, array[i]) === 2) {
-      return true;
-    } else if (callback(array, array[i]) === 0){
-      return false;
-   } else {
-      return false;
-  }
-    }
+function hofall(list, value, callback){
+	for (var i = 0; i < list.length;i++){
+		if (callback(list[i], value) === false) {
+			return false;
+		}
+	}
+	return true;
 }
+
