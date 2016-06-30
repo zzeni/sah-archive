2948c3d0df3812d0fcb0c44e65e11f8a6e82a0b3
Acrelion <iachi1763@gmail.com>
2016-07-01 00:03:01 +0300

    Final project V4
    Form to email, new style for the modal, js validation for the contact
    form. Including the translation.

--- ivan/project/js/script.js
+++ ivan/project/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     // My namespace.
@@ -19,18 +19,48 @@ $(document).ready(function() {
     common.englishTranslation = {};
     common.bulgarianTranslation = {};
     // Common function for adding the generated elements by the menu and the gallery.
-    common.appendItems = function(parent, itemsList) {
-        itemsList.forEach(function(item) {
+    common.appendItems = function (parent, itemsList) {
+        itemsList.forEach(function (item) {
             parent.append(item.generateHTML());
         });
     };
 
-    (function cookieManagement() {
+
+
+
+    (function cookieManagementAndGenerate() {
+
+
+        function GalleryItemClass(url, alt) {
+            this.url = url;
+            this.alt = alt;
+        }
+        GalleryItemClass.prototype.generateHTML = function () {
+            var galleryElementWrapper = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb">' + '<div class="thumbnail">' +
+                '<img class="img-responsive gallery-item" src="' + this.url + '" alt="' + this.alt + '">' + '</div>' + '</div>';
+            return galleryElementWrapper;
+        };
+
+
+        function MenuItemClass(title, description) {
+            this.title = title;
+            this.description = description;
+        }
+        MenuItemClass.prototype.generateHTML = function () {
+            var menuElementWrapper = '<div class="col-sm-6 col-md-12 menuItem">' + '<h4 class="dishTitle">' + this.title + '</h4>' + '<p>' + this.description +
+                '</p>' + '</div>';
+            return menuElementWrapper;
+        };
+
+
+
+
+
+
         // Cookie management code
 
         loadCookies(common.cookies);
         common.cookieLanguage = common.cookies.language;
-
 
 
         if (common.cookieLanguage && (common.cookieLanguage === "bg" || common.cookieLanguage === "en")) {
@@ -38,38 +68,42 @@ $(document).ready(function() {
 
             $(".lang_select").removeClass("active_lang");
             $('#' + common.cookieLanguage).addClass("active_lang");
-        }
-		else {
-			common.cookieLanguage === "en";
-			
-			$('html').attr('lang', common.cookieLanguage);
-			$(".lang_select").removeClass("active_lang");
+        } else {
+            common.cookieLanguage = "en";
+
+            $('html').attr('lang', common.cookieLanguage);
+            $(".lang_select").removeClass("active_lang");
             $('#' + common.cookieLanguage).addClass("active_lang");
-		}
-		
-		
-		
-		if ((localStorage.getItem("englishTranslationObj") !== null) && (localStorage.getItem("bulgarianTranslationObj") !== null)) {
-            common.englishTranslation = localStorage.getItem("englishTranslationObj");
-            common.bulgarianTranslation = localStorage.getItem("bulgarianTranslationObj");
+        }
 
-            applyTranslation();
-		} else {
-			$.getJSON('js/langs.json', function(data) {
 
-				common.englishTranslation = data.en;
-				common.bulgarianTranslation = data.bg;
 
-				localStorage.setItem("englishTranslationObj", common.englishTranslation);
-				localStorage.setItem("bulgarianTranslationObj", common.bulgarianTranslation);
+        /* if ((localStorage.getItem("englishTranslationObj") !== null) && (localStorage.getItem("bulgarianTranslationObj") !== null)) {
 
-			}).then(applyTranslation);
-		}
-		
+
+             //common.englishTranslation = localStorage.getItem("englishTranslationObj");
+             //common.bulgarianTranslation = localStorage.getItem("bulgarianTranslationObj");
+
+
+
+             applyTranslation();
+         } else {*/
+
+        $.getJSON('js/langs.json', function (data) {
+
+            common.englishTranslation = data.en;
+            common.bulgarianTranslation = data.bg;
+
+            //localStorage.setItem("englishTranslationObj", data["en"]);
+            //localStorage.setItem("bulgarianTranslationObj", common.bulgarianTranslation);
+
+        }).then(applyTranslation);
+
+
 
 
         // Event handler for switching the language.
-        $(".lang_select").click(function() {
+        $(".lang_select").click(function () {
             var lang = $(this).attr("id");
 
             $(".lang_select").removeClass("active_lang");
@@ -78,14 +112,18 @@ $(document).ready(function() {
             $('html').attr('lang', lang);
             setCookie('language', lang);
             common.cookieLanguage = lang;
-            applyTranslation();
+
+            if (common.englishTranslation) {
+                applyTranslation();
+            }
+
         });
 
 
 
         // Load and check the the previously applied cookie.
         function loadCookies(cookieHash) {
-            document.cookie.split('; ').forEach(function(keyvalStr) {
+            document.cookie.split('; ').forEach(function (keyvalStr) {
                 var keyval = keyvalStr.split('=');
                 cookieHash[keyval[0]] = keyval[1];
             });
@@ -100,51 +138,101 @@ $(document).ready(function() {
 
             document.cookie = key + "=" + val + "; expires=" + expires + "; path=/";
 
-            console.log(document.cookie);
+            //console.log(document.cookie);
         }
-		
-		
-		
-		
+
+
+
+
 
         // Code for reading the json and applying the translation
 
         function applyTranslation() {
-				
-			var typesOfDataTags = ["data-nav-ts", "data-carousel-ts", "data-shortinfo-ts",
-			"data-header-ts", "data-menu-ts", "data-reservations-ts", "data-about-ts", "data-contact-ts"];	
+
+            var typesOfDataTags = ["data-nav-ts", "data-carousel-ts", "data-shortinfo-ts",
+                                   "data-header-ts", "data-menu-ts", "data-reservations-ts", "data-about-ts", "data-contact-ts"];
+
+            var sources = ["navigation", "carousel", "shortinfo", "section_headers", "menu", "reservations", "about_us", "contact"];
+
 
             if (common.cookieLanguage === "bg") {
-				
-				
-				
-				$("[data-nav-ts]").each(function(){
-					$(this).html(common.bulgarianTranslation["navigation"][$(this).attr("data-nav-ts").toString()]);
-				});
-				
-				
-				
-                $("[data-translate]").each(function() {
-                    $(this).html(common.bulgarianTranslation[$(this).attr("data-translate")].toString());
-                });
-				
-				
-				
-				
+
+                applyAndGenerate(common.bulgarianTranslation);
+
+
             } else {
-                $("[data-translate]").each(function() {
-                    $(this).html(common.englishTranslation[$(this).attr("data-translate").toString()]);
+
+                applyAndGenerate(common.englishTranslation);
+
+            }
+
+            function applyAndGenerate(languageSource) {
+                for (var i = 0; i < typesOfDataTags.length; i++) {
+                    applyTranslationForType(typesOfDataTags[i], languageSource[sources[i].toString()]);
+                }
+
+                generateGallery(languageSource.gallery);
+
+                generateMenu(languageSource.menu.breakfast, languageSource.menu.lunch, languageSource.menu.dinner);
+            }
+
+
+
+            function applyTranslationForType(dataTag, source) {
+
+                $("[" + dataTag + "]").each(function () {
+                    $(this).html(source[$(this).attr(dataTag).toString()]);
                 });
             }
-			
-			
-			function applyTranslationForType(dataTag, )
-			
-			
-			
-        }
 
-        
+            function generateGallery(source) {
+                var galleryDB = [];
+                var galleryParent = $("#galleryThumbsContainer");
+
+                galleryParent.html("");
+
+
+
+                source.forEach(function (obj) {
+                    var thumbnailObj = new GalleryItemClass(obj.url, obj.alt);
+                    galleryDB.push(thumbnailObj);
+                });
+
+                $.when(common.appendItems(galleryParent, galleryDB)).done(callModal);
+            }
+
+            function generateMenu(source1, source2, source3) {
+                var menuDBs = [[], [], []];
+
+
+                var menuParents = [$("#menucol1"), $("#menucol2"), $("#menucol3")];
+
+                menuParents[0].html("");
+                menuParents[1].html("");
+                menuParents[2].html("");
+
+
+                function createMenuObjects(obj) {
+                    var menuItemObject = new MenuItemClass(obj.title, obj.description);
+                    return menuItemObject;
+                }
+
+                source1.forEach(function (obj) {
+                    menuDBs[0].push(createMenuObjects(obj));
+                });
+                source2.forEach(function (obj) {
+                    menuDBs[1].push(createMenuObjects(obj));
+                });
+                source3.forEach(function (obj) {
+                    menuDBs[2].push(createMenuObjects(obj));
+                });
+
+                common.appendItems(menuParents[0], menuDBs[0]);
+                common.appendItems(menuParents[1], menuDBs[1]);
+                common.appendItems(menuParents[2], menuDBs[2]);
+            }
+
+        }
 
     }());
 
@@ -153,15 +241,16 @@ $(document).ready(function() {
         $("#myCarousel").carousel({
             interval: 5000
         });
-        $("#carousel-pause").click(function() {
+        $("#carousel-pause").click(function () {
             $("#myCarousel").carousel('pause');
         });
-        $("#carousel-play").click(function() {
+        $("#carousel-play").click(function () {
             $("#myCarousel").carousel('cycle');
         });
     }
-    // Add smooth scrolling to all links in navbar + footer link
-    $("a[href*='#']").on('click', function(event) {
+
+    // Add smooth scrolling to all links in navbar + floating button
+    $("a[href*='#']").on('click', function (event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
@@ -177,126 +266,49 @@ $(document).ready(function() {
             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function() {
+            }, 800, function () {
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
         } // End if
     });
-    $(window).scroll(function() {
-        $(".slideanim").each(function() {
+
+    // For sliding in elements.
+    $(window).scroll(function () {
+        $(".slideanim").each(function () {
             var pos = $(this).offset().top;
             var winTop = $(window).scrollTop();
             if (pos < winTop + 600) {
                 $(this).addClass("slide");
             }
         });
+
+
+        ($(this).scrollTop() > common.offset) ? common.$back_to_top.addClass('cd-is-visible'): common.$back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > common.offset_opacity) {
+            common.$back_to_top.addClass('cd-fade-out');
+        }
     });
+
+
     // For gallery modal.
     function callModal() {
-        $('.gallery-item').click(function(event) {
+        $('.gallery-item').click(function (event) {
             event.preventDefault();
             $('.imagepreview').attr('src', $(this).attr('src'));
             $("#modalAltText").text($(this).attr('alt'));
             $('#imagemodal').modal('show');
         });
+
+        // For closing gallery modal with mouse click.
+        $("#imagemodal").click(function () {
+            $(this).modal("hide");
+        });
     }
-    // For closing gallery modal with mouse click.
-    $("#imagemodal").click(function() {
-        $(this).modal("hide");
-    });
-    (function GalleryHandler() {
-        function GalleryItemClass(url, alt) {
-            this.url = url;
-            this.alt = alt;
-        }
-        GalleryItemClass.prototype.generateHTML = function() {
-            var galleryElementWrapper = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb">' + '<div class="thumbnail">' +
-                '<img class="img-responsive gallery-item" src="' + this.url + '" alt="' + this.alt + '">' + '</div>' + '</div>'
-            return galleryElementWrapper;
-        };
-        $.getJSON('js/datanew.json', function(data) {
-            var galleryDB = [];
-            var galleryParent = $("#galleryContainer");
 
 
-            /* if (common.cookieLanguage === "en"){
-                    data.forEach(function(obj){
-                        var thumbnailObj = new GalleryItemClass(obj.url, common.englishTranslation[gallery][obj.altKey])
-                });
-                }
-
-            */
-
-
-            data.forEach(function(obj) {
-                var thumbnailObj = new GalleryItemClass(obj.url, obj.alt);
-                galleryDB.push(thumbnailObj);
-            });
-            common.appendItems(galleryParent, galleryDB);
-        }).then(callModal);
-        /* Every time the window is scrolled ... */
-        $(window).scroll(function() {
-            /* Check the location of each desired element */
-            $('.hideme').each(function(i) {
-                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                /* If the object is completely visible in the window, fade it it */
-                if (bottom_of_window > bottom_of_object) {
-                    $(this).animate({
-                        'opacity': '1'
-                    }, 500);
-                }
-            });
-        });
-    }()); // galleryHandler
-    /////////////// Menu handler
-    (function menuHandler() {
-        function MenuItemClass(title, description) {
-            this.title = title;
-            this.description = description;
-        }
-        MenuItemClass.prototype.generateHTML = function() {
-            var menuElementWrapper = '<div class="col-sm-6 col-md-12 menuItem">' + '<h4 class="dishTitle">' + this.title + '</h4>' + '<p>' + this.description +
-                '</p>' + '</div>'
-            return menuElementWrapper;
-        };
-        $.getJSON('js/datamenu.json', function(data) {
-            var menuDBs = [
-                [],
-                [],
-                []
-            ];
-            var menuParents = [$("#menucol1"), $("#menucol2"), $("#menucol3")];
-
-            function createMenuObjects(obj) {
-                var menuItemObject = new MenuItemClass(obj.title, obj.description);
-                return menuItemObject;
-            }
-
-            data.breakfast.forEach(function(obj) {
-                menuDBs[0].push(createMenuObjects(obj))
-            });
-            data.lunch.forEach(function(obj) {
-                menuDBs[1].push(createMenuObjects(obj))
-            });
-            data.dinner.forEach(function(obj) {
-                menuDBs[2].push(createMenuObjects(obj))
-            });
-
-
-            /*objwrapper.forEach(function(obj) {
-                    var menuItemObject = new MenuItemClass(obj.title, obj.description);
-                    menuDBs[data.indexOf(objwrapper)].push(menuItemObject);
-            });*/
-
-            common.appendItems(menuParents[0], menuDBs[0]);
-            common.appendItems(menuParents[1], menuDBs[1]);
-            common.appendItems(menuParents[2], menuDBs[2]);
-        });
-    }());
     // Date picker in Reservations
-    $(function() {
+    $(function () {
         var today = new Date();
         $('#datetimepicker12').datetimepicker({
             inline: true,
@@ -306,11 +318,8 @@ $(document).ready(function() {
     });
 
     //hide or show the "back to top" link
-    $(window).scroll(function() {
-        ($(this).scrollTop() > common.offset) ? common.$back_to_top.addClass('cd-is-visible'): common.$back_to_top.removeClass('cd-is-visible cd-fade-out');
-        if ($(this).scrollTop() > common.offset_opacity) {
-            common.$back_to_top.addClass('cd-fade-out');
-        }
+    $(window).scroll(function () {
+
     });
 
 
@@ -326,6 +335,128 @@ $(document).ready(function() {
         });
     }
 
+
+	(function contactFormHandler(){
+		
+		function checkInput(id, value){
+			var nameRE =  /\b([A-Z]{1}[a-z]{1,30}[- ]{0,1}|[A-Z]{1}[- \']{1}[A-Z]{0,1}[a-z]{1,30}[- ]{0,1}|[a-z]{1,2}[ -\']{1}[A-Z]{1}[a-z]{1,30}){2,5}/  // old /^[a-zA-Z0-9_-]{4,10}$/;
+			var phoneRE = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+			var emailRE = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+			
+			var everythingOk = true;
+			
+			if (id === "sender_name") {
+				if ( !Boolean(value.match(nameRE)) ) {
+					everythingOk = false;
+				}
+			}
+			else if (id === "telnum" && value !== "") {
+				if ( !Boolean(value.match(phoneRE)) ) {
+					everythingOk = false;
+				}
+			}
+			else if (id === "emailid") {
+				if ( !Boolean(value.match(emailRE)) ) {
+					everythingOk = false;
+				}
+			}
+			else if (id === "contact_message") {
+				if (value.length < 3) {
+					everythingOk = false;
+				}
+			}
+			
+			return everythingOk;
+		}
+		
+		
+		
+		
+		function setError(parent) {
+			parent.addClass('has-error');
+			parent.find('.error').show();
+			$("#contact_submit_btn").prop('disabled', true);
+		}
+
+		function good2go(parent) {
+			parent.addClass('has-success');
+			parent.find('.error').hide();
+		}
+
+		function cleanClass(parent) {
+			$("#contact_submit_btn").prop('disabled', false);
+			
+			if (parent.hasClass("has-error")) {
+				parent.removeClass("has-error");
+				parent.find(".error").hide();
+			}
+			if (parent.hasClass("has-success")) {
+				parent.removeClass("has-success");
+			}
+		}
+		
+
+		
+		$("#contact_form input, #contact_message").blur(function(){
+			var value = $(this).val();
+			var id = $(this).attr("id");
+			var inputParent = $(this).parent();
+			
+			
+			
+			if (checkInput(id, value) === true && value !== "") { // if everything is ok
+				cleanClass(inputParent);
+				good2go(inputParent);
+			}
+			else if (checkInput(id, value) === false && value !== "") {
+				cleanClass(inputParent);
+				setError(inputParent);
+			}
+			else {
+				cleanClass(inputParent);
+			}
+			
+		});	
+		
+		
+		function contactSubmitHandler() {
+		
+			$.ajax({
+				url: $("#contact_form").attr("action"),
+				type: $("#contact_form").attr("method"),
+				data: $("#contact_form").serialize(),
+				cache: false,
+				error: function (e) {
+					console.log(e);
+				}
+			});
+		}
+
+		$("#contact_form").on('submit', function (ev) {
+			
+			//if ( $("#contact_form").find('.error').length !== 0  ) { // if there are no errors
+			
+				contactSubmitHandler();
+				ev.preventDefault();	
+
+				document.getElementById("contact_form").reset();
+				
+				$(".has-success, .has-error").removeClass("has-success has-error");
+				$("#message_sent").addClass("message_sent_animation");
+				
+			
+			
+			ev.preventDefault()
+			
+
+		});
+			
+	}());
+	
+	
+
+	
+	
 
 
 
